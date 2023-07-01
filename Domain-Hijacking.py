33a1abe5c7ef8bb1b07e88ebb1dc7d93e3c376a1
SpaghettiSec <105888772+spaghettisec@users.noreply.github.com>
2023-07-01 16:22:18 -0700

    Update and rename Dangling-CNAME.py to Domain-Hijacking.py

--- Domain-Hijacking.py
+++ Domain-Hijacking.py
@@ -3,27 +3,26 @@ from typing import List
 from sys import argv, stdin
 from termcolor import colored
 from socket import gethostbyname
-from dns.resolver import resolve, query
+from dns.resolver import resolve, query, NXDOMAIN
 
-def nxdomain(domain: str) -> bool:
+def NXDomain(domain: str) -> bool:
     try:
-        query(domain)
+        resolve(domain)
         return False
-    except:
+    except NXDOMAIN:
         return True
-
 
 if __name__ == '__main__':
     if stdin.isatty():
-        print("echo \"example.com\" | python3 Dangling-CNAME.py")
+        print("echo \"example.com\" | python3 Domain-Hijacking.py")
     else:
         domains: List[str] = stdin.read().strip().split("\n")
         for domain in domains:
             try:
-                answers =  resolve(domain, "CNAME")      
+                answers = resolve(domain, "CNAME")      
                 cname: str = answers[0].to_text()
-                if len(cname) > 0 and nxdomain(domain):
-                    print(f"[{colored('Dangling CNAME', 'green')}] {colored(domain, 'yellow')} [{colored(cname, 'blue')}]")
+                if NXDomain(domain):
+                    print(f"[{colored('NXDOMAIN', 'green')}] {colored(domain, 'yellow')} [{colored(cname, 'blue')}]")
             except KeyboardInterrupt:
                 break
             except:
